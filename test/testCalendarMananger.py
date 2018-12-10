8df5da94796d5dbbf2690f733b5e967d930ead0b
Angheng <bungae1112@gmail.com>
2018-12-11 01:19:30 +0900

    fix blank lines

--- test/testCalendarMananger.py
+++ test/testCalendarMananger.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 from calendarManager import MyCalendar, MyEvent
 
+
 class TestCalendarManager(unittest.TestCase):
     def setUp(self):
         self.myCalendar = MyCalendar()
